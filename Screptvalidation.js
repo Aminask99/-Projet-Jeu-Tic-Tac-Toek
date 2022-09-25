59e08a7d73d10d55a2d20329cffdfff994f31196
amina <aminasalik012@gmail.com>
2022-09-25 02:39:21 +0100

    local Storage done

--- Screptvalidation.js
+++ Screptvalidation.js
@@ -1,16 +1,81 @@
-function enter(){
-                                        
-    let Player1 = document.getElementById("Player1").value;
-    let Player2 = document.getElementById("Player2").value;
+const formEnter = e =>{
+    let user1 =document.getElementById("Player1").value;
+    let user2 =document.getElementById("Player2").value;
+    e.preventDefault();
+            localStorage.setItem("Player1",user1);
+            localStorage.setItem("Player2",user2);
 
-            localStorage.setItem("user1",Player1)
-            localStorage.setItem("user2",Player2)
-
-                if(!Player1 =="" && !Player2 == ""){
-                
+            if( !user1 =="" && !user2 == ""){
                 window.open('xO.html');
-              
-                }
 
+            }
+
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function enter(e){
+                                        
+//     let Player1 = document.getElementById("Player1").value;
+//     let Player2 = document.getElementById("Player2").value;
+//        h1 = document.querySelector("h1");
+//        h1.innerHTML=localStorage.getItem('user1')
+//        h2 = document.querySelector("h2");
+      
+//        h2.innerHTML=localStorage.getItem('user2')
+
+//             localStorage.setItem("user1",Player1)
+//             localStorage.setItem("user2",Player2)
+
+//             console.log(localStorage.getElementBy('user1'));
+//             e.p
+//             h1.innerHTML=localStorage.getItem('user1')
+//             h2.innerHTML=localStorage.getItem('user2')
+
+//                 if(!Player1 =="" && !Player2 == ""){
+                
+//                 window.open('xO.html');
+                
+              
+//                 }
+//         }
+       
+            
+           
--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
+      if(localStorage.getItem("Player1")  ){
+        Player1 = localStorage.getItem("Player1");
+        Player2 = localStorage.getItem("Player2");
+        console.log(Player1 );
+        console.log(Player2 );
 
-
+        output.innerHTML =
+            ` <h1 class=" d-flex justify-content-center"> ${Player1} vs  ${Player2}  </h1> 
+             ` ;                
+        
+         }
+        
+         document.getElementById("name").textContent=Player2;
+      
 
 
 
 let buttons = document.getElementById("buttons");
 let divent = document.getElementById("WinnMES");
 let diven = document.getElementById("WinnME");
+let play = document.getElementById("play");
+
 let music = document.getElementById("music");
+let sunGame = document.getElementById("sunGame");
+let win = document.getElementById("win");
+let lose = document.getElementById("lose");
 function PlayMusic(){
     music.play();
 
@@ -22,17 +39,25 @@ let turn = true;
  arrayO=[];
  array=[];
  
-// divent.addEventListener('click',(Event)=>{
-//     document.getElementById("WinnMES").style.display = "none";
-// })
+
+
 
 buttons.addEventListener('click',(Event)=>{
     Event.preventDefault();
+    
+   
     let button = Event.target.className
     if (!button.includes("joux"))
     {
+      
         return
     }
+    if (button.includes("joux"))
+    {
+      
+        sunGame.play()
+    }
+
    
     if(!Event.target.textContent=="" ){
         return
@@ -42,12 +67,14 @@ buttons.addEventListener('click',(Event)=>{
         Event.target.textContent="x";
         arrayX.push(Event.target.id)
         array.push(Event.target.id)
+        document.getElementById("name").textContent=Player1;
         turn=false
     }else{
         Event.target.style.background="green";
         Event.target.textContent="o"; 
         arrayO.push(Event.target.id)
         array.push(Event.target.id)
+        document.getElementById("name").textContent=Player2;
         turn=true
     }
    
@@ -57,43 +84,60 @@ buttons.addEventListener('click',(Event)=>{
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
-
+      win.play();       
+        return
         // document.getElementById("WinnMES").textContent="X Wins !"
     }
     if(arrayX.includes('4') && arrayX.includes('5') && arrayX.includes('6')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+       
+     win.play();     
+        return
     }
     if(arrayX.includes('7') && arrayX.includes('8') && arrayX.includes('9')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        
+     win.play();     
+      return
     }
     if(arrayX.includes('1') && arrayX.includes('4') && arrayX.includes('7')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        win.play();
+        return
     }
     if(arrayX.includes('2') && arrayX.includes('5') && arrayX.includes('8')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        win.play();
+        return
     }
     if(arrayX.includes('3') && arrayX.includes('6') && arrayX.includes('9')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        win.play();
+        return
     }
     if(arrayX.includes('1') && arrayX.includes('5') && arrayX.includes('9')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        win.play();
+        return
     }
     if(arrayX.includes('3') && arrayX.includes('5') && arrayX.includes('7')  )
     {
         document.getElementById("WinnME").style.display = "block";
         document.getElementById("CloseBtn").style.display = "block";
+        win.play();
+        return
     }
 
 
@@ -104,6 +148,8 @@ buttons.addEventListener('click',(Event)=>{
         
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
         // document.getElementById("WinnMES").style.display = "flex";
 
     }
@@ -112,55 +158,70 @@ buttons.addEventListener('click',(Event)=>{
        
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('7') && arrayO.includes('8') && arrayO.includes('9')  )
     {
        
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('1') && arrayO.includes('4') && arrayO.includes('7')  )
     {
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('2') && arrayO.includes('5') && arrayO.includes('8')  )
     {
        
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('3') && arrayO.includes('6') && arrayO.includes('9')  )
     {
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('1') && arrayO.includes('5') && arrayO.includes('9')  )
     {
      
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
     }
     if(arrayO.includes('3') && arrayO.includes('5') && arrayO.includes('7')  )
     {
        
         document.getElementById("WinnMES").style.display = "block";
         document.getElementById("CloseBt").style.display = "block";
+        win.play();
+        return
 
  
     
 
     }
-    if (array.includes('1')&&array.includes('2')&&array.includes('3')&&array.includes('4')&&array.includes('5')&&array.includes('6')&&array.includes('7')&&array.includes('8')&&array.includes('9')){
-        console.log("nooooooooo");
+
+    if (array.includes('1')&&array.includes('2')&&array.includes('3')&&array.includes('4')&&array.includes('5')&&array.includes('6')&&array.includes('7')&&array.includes('8')&&array.includes('9') ){
+        lose.play();
+        // console.log("nooooooooo");
         document.getElementById("WinnM").style.display = "block";
         document.getElementById("closeOPS").style.display = "block";
+       
 
     }
 
-    // if (array==9){
-    //     console.log(ta3adoll);
-    // }  
+   
 })
 // Close X
 function close_(){
